21a7fece57b60489c527bf17db4f4bc69ee31175
darimi100 <105377694+darimi100@users.noreply.github.com>
2023-03-30 18:04:44 +0900

    [Unrated] Title: 문자열 반복, Time: 220 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Unrated/2675. 문자열 반복/문자열 반복.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int cnt = sc.nextInt();
+        for(int i = 0; i < cnt; i++) {
+            
+            int n = sc.nextInt();
+            String s = sc.next();
+            
+            for(int j = 0; j < s.length(); j++) {
+                for(int k = 0; k < n; k++) {
+                    System.out.print(s.charAt(j));
+                }
+            }
+            System.out.println();
+        }
+    }
+}
