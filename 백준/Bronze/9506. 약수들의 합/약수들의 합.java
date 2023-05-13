91dec0bb9466212299bbd4a0761f128f39954fd4
darimi100 <105377694+darimi100@users.noreply.github.com>
2023-05-13 15:41:45 +0900

    [Bronze I] Title: 약수들의 합, Time: 240 ms, Memory: 18232 KB -BaekjoonHub

---
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        while(true) {
+            int n = sc.nextInt();
+            if(n == -1) {
+                break;
+            }
+            
+            ArrayList<Integer> list = new ArrayList<>();
+            int sum = 0;
+            
+            for(int i = 1; i < n; i++) {
+                if(n % i == 0) {
+                    list.add(i);
+                }
+            }
+            
+            for(int i = 0; i < list.size(); i++) {
+                sum += list.get(i);
+            }
+            
+            if(n == sum) {
+                System.out.print(n + " = ");
+                for(int i = 0; i < list.size(); i++) {
+                    System.out.print(list.get(i));
+                    if(i < list.size() - 1) {
+                        System.out.print(" + ");
+                    }
+                }
+                System.out.println();
+            }
+            else {
+                System.out.println(n + " is NOT perfect.");
+            }
+        }
+        
+    }
+}
