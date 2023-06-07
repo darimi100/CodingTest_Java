1f7d5d5f16dce2f1e575bfe5003db3c4bd492778
darimi100 <105377694+darimi100@users.noreply.github.com>
2023-06-08 06:07:39 +0900

    [Silver V] Title: 영화감독 숌, Time: 384 ms, Memory: 89296 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 1;
+        int num = 666;
+        
+        while (count != n) {
+            num++;
+            if(String.valueOf(num).contains("666")) {
+                count++;
+            }
+        }
+        
+        System.out.print(num);
+    }
+}
