113a1820e726eb045e7a147cecba6c4e7534de43
darimi100 <105377694+darimi100@users.noreply.github.com>
2023-06-09 19:03:21 +0900

    다른 방식 추가

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -4,7 +4,26 @@ class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int count = 0;
         
+        while(n % 5 != 0) {
+            n -= 3;
+            if(n < 0) {
+                count = -1;
+                break;
+            }
+            else
+                count++;
+        }
+        
+        if(n > 0) {
+            count += (n/5);
+        }
+        
+        System.out.println(count);
+        
+        
+        /*
         if (n == 4 || n == 7) {
 			System.out.println(-1);
 		}
@@ -17,5 +36,6 @@ class Main {
 		else if (n % 5 == 2 || n % 5 == 4) {
 			System.out.println((n / 5) + 2);
 		}
+        */
     }
 }
